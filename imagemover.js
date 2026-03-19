0eb61efd641a58156288412a088cb82c2e66d893
Cool_Bungle <carderskaggz@gmail.com>
2026-03-19 08:24:13 -0600

    Create imagemover.js

---
+++ imagemover.js
@@ -0,0 +1,24 @@
+const img = document.getElementById("draggable");
+
+let isDragging = False;
+let offsetX, offsetY;
+img.addEventListener("mousedown", (e) => {
+  isDragging = true;
+
+  offsetX = e.clientX - img.offsetLeft;
+  offsetY = e.clientY - img.offsetTop;
+
+  img.style.cursor = "grabbing";
+});
+
+document.addEventListener("mousemove", (e) => {
+  if(!isDragging) return;
+
+  img.style.left = (e.clientX - offsetX) + "px";
+  img.style.top = (e.clientY - offsetY) + "px";
+});
+
+document.addEventListener("mouseup", () => {
+  isDragging = false;
+  img.style.cursor = "grab";
+});
