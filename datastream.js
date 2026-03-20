6fc1f03c9a7a9f5b4f8489a70c521fed12b4f9d5
Cool_Bungle <carderskaggz@gmail.com>
2026-03-20 10:54:46 -0600

    Added datastream
    Enhance cycleDataStream function to display random phrases with fade effect.

---
+++ datastream.js
@@ -0,0 +1,21 @@
+const phrases = [
+  "guh"
+  "wazzup"
+  "splash text"
+  "click me"
+];
+
+const datastream = document.getElementById("datastream");
+
+function cycleDataStream() {
+  const text = phrases[Math.floor(Math.random() * phrases.length)];
+
+  datastream.textContent = text;
+  datastream.style.opacity = 1;
+
+  setTimeout(() => {
+    datastream.style.opacity = 0;
+  }, 2000);
+}
+
+setInterval(cycleDataStream, 4000);
