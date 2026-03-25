16370e5501e8a994df7de0df781ee51b3526771b
Cool_Bungle <carderskaggz@gmail.com>
2026-03-25 11:21:26 -0600

    Update push.js

--- push.js
+++ push.js
@@ -1,13 +1,20 @@
-window.send = async function () {
-  const message = document.getElementById("msg").value;
+async function sendMessage(message) {
+  try {
+    await fetch("https://spring-scene-7a88.carderskaggz.workers.dev", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ message })
+    });
 
-  await fetch("https://spring-scene-7a88.carderskaggz.workers.dev", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ message: message })
-  });
+    const line = document.createElement("div");
+    line.innerHTML = `<span style="color:lightgreen">sent: "${message}"</span>`;
+    output.appendChild(line);
 
-  alert("Sent");
-};
+  } catch (err) {
+    const line = document.createElement("div");
+    line.innerHTML = `<span style="color:red">failed to send</span>`;
+    output.appendChild(line);
+  }
+}
