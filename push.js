26c61216cae9a352df301ad9452bf1e2c8412332
Cool_Bungle <carderskaggz@gmail.com>
2026-03-25 10:22:59 -0600

    Change webhook URL in send function

--- push.js
+++ push.js
@@ -1,7 +1,7 @@
     async function send() {
       const message = document.getElementById("msg").value;
 
-      await fetch("https://discord.com/api/webhooks/1486392154401017901/QzclzfTWdNq5E4Sye7MvATf1CFsW5Yno_eenhPM7kvVhYhl03OcJE0xdmXxr4unO0OpC", {
+      await fetch("https://spring-scene-7a88.carderskaggz.workers.dev/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
