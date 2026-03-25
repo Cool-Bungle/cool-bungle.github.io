0d99c21356580551437222a3ab1336282d840f8a
Cool_Bungle <carderskaggz@gmail.com>
2026-03-25 09:53:02 -0600

    Add function to send push notifications

---
+++ push.js
@@ -0,0 +1,14 @@
+ <script>
+    async function send() {
+      const message = document.getElementById("msg").value;
+
+      await fetch("https://discord.com/api/webhooks/1486392154401017901/QzclzfTWdNq5E4Sye7MvATf1CFsW5Yno_eenhPM7kvVhYhl03OcJE0xdmXxr4unO0OpC", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ message })
+      });
+
+      alert("sent");
+    }
