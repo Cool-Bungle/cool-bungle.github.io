8e4f44de76fdf1fbf88a1938c22704714a7ac46b
Cool_Bungle <carderskaggz@gmail.com>
2026-03-25 10:35:37 -0600

    Refactor send function to be a global function

--- push.js
+++ push.js
@@ -1,13 +1,13 @@
-    async function send() {
-      const message = document.getElementById("msg").value;
+window.send = async function () {
+  const message = document.getElementById("msg").value;
 
-      await fetch("https://spring-scene-7a88.carderskaggz.workers.dev", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ message })
-      });
+  await fetch("https://spring-scene-7a88.carderskaggz.workers.dev/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ message: message })
+  });
 
-      alert("sent");
-    }
+  alert("sent");
+};
