b350cfb96ac2c99800f950edd649d206d6b312ec
Cool_Bungle <carderskaggz@gmail.com>
2026-03-25 10:32:59 -0600

    Fix fetch URL syntax in send function

--- push.js
+++ push.js
@@ -1,7 +1,7 @@
     async function send() {
       const message = document.getElementById("msg").value;
 
-      await fetch("https://spring-scene-7a88.carderskaggz.workers.dev/"), {
+      await fetch("https://spring-scene-7a88.carderskaggz.workers.dev", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
